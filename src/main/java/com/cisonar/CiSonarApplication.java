17a49f46e7bf84f8d91b9eaf8486f14d9b41cbf9
Renan Morais <renanc.morais@gmail.com>
2022-05-08 22:40:11 -0300

    first commit

---
+++ src/main/java/com/cisonar/CiSonarApplication.java
@@ -0,0 +1,13 @@
+package com.cisonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiSonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiSonarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cisonar/CiSonarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cisonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiSonarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
